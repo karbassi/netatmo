d5545e7f1b7795c57b9c38a703bd4e0b1f18be4b
floetenbaer <38409313+floetenbaer@users.noreply.github.com>
2022-01-21 16:11:07 +0100

    new unit tests

--- test/test.js
+++ test/test.js
@@ -4,7 +4,8 @@ const process = require('process');
 const test = require('ava');
 const netatmo = require('../netatmo');
 // eslint-disable-next-line ava/no-import-test-files
-const { getCredentials, getWeatherDeviceId, getEnergyDeviceId, getRoomId } = require('./credentials');
+const { getCredentials, getTestParameters } = require('./credentials');
+const regexMacAddr = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9a-fA-F]{4}\\.[0-9a-fA-F]{4}\\.[0-9a-fA-F]{4})$/;
 
 // @ts-ignore
 // test produces a "Uncaught exception in test.js" 
@@ -40,21 +41,27 @@ test.before('authenticate', t => {
 
 function apiCallAsync(api, func, options = null) {
     return new Promise(function (resolve, reject) {
-        // @ts-ignore
-        api.on("error", function (error) {
+        function errorWarningHandler(error) {
             reject(error.message);
-        });
+        }
         // @ts-ignore
-        api.on("warning", function (error) {
-            reject(error.message);
-        });
+        api.on("error", errorWarningHandler);
+        // @ts-ignore
+        api.on("warning", errorWarningHandler);
+
+        function uncaughtExceptionHandler(reason) {
+            reject(reason.message);
+        }
         // @ts-ignore
         // eslint-disable-next-line no-unused-vars
-        process.on('uncaughtException', (reason, promise) => {
-            // console.error(`Uncaught error in`, promise);
-            reject(reason.message);
-        });
+        process.on('uncaughtException', uncaughtExceptionHandler);
+
         func.bind(api)(options, function (err, data) {
+            process.removeListener('uncaughtException', uncaughtExceptionHandler);
+            // @ts-ignore
+            api.removeListener('error', errorWarningHandler);
+            // @ts-ignore
+            api.removeListener('warning', errorWarningHandler);
             if (err !== null) reject(err);
             else resolve(data);
         });
@@ -79,6 +86,44 @@ test.serial('homesData with invalid home_id', t => {
 });
 
 // @ts-ignore
+test.serial('getStationsData without device_id', t => {
+    return apiCallAsync(t.context.api, t.context.api.getStationsData).then(result => {
+        // console.log(result);
+        t.assert(result);
+        t.assert(Array.isArray(result));
+    }).catch(error => { t.is(error, ''); });
+});
+
+if (getTestParameters().weatherDeviceId) {
+    // @ts-ignore
+    test.serial('getStationsData with device_id', t => {
+        const options = {
+            device_id: getTestParameters().weatherDeviceId,
+        };
+        return apiCallAsync(t.context.api, t.context.api.getStationsData, options).then(result => {
+            t.assert(result);
+            t.assert(Array.isArray(result));
+            t.regex(result[0]._id, regexMacAddr, "[0]._id is not a mac address")
+        }).catch(error => { t.is(error, ''); });
+    });
+}
+
+if (getTestParameters().weatherDeviceId) {
+    // @ts-ignore
+    test.serial('getStationsData with device_id and get_favorites', t => {
+        const options = {
+            device_id: getTestParameters().weatherDeviceId,
+            get_favorites: true,
+        };
+        return apiCallAsync(t.context.api, t.context.api.getStationsData, options).then(result => {
+            t.assert(result);
+            t.assert(Array.isArray(result));
+            t.regex(result[0]._id, regexMacAddr, "[0]._id is not a mac address")
+        }).catch(error => { t.is(error, ''); });
+    });
+}
+
+// @ts-ignore
 test.serial('getMeasure without device_id', t => {
     return apiCallAsync(t.context.api, t.context.api.getMeasure, {}).then(result => {
         t.assert(result);
@@ -87,27 +132,28 @@ test.serial('getMeasure without device_id', t => {
 });
 
 
-if (getWeatherDeviceId() !== null) {
+if (getTestParameters().weatherDeviceId) {
     // @ts-ignore
     test.serial('getMeasure with scale max and complete type array', t => {
         const options = {
-            device_id: getWeatherDeviceId(),
+            device_id: getTestParameters().weatherDeviceId,
             scale: 'max',
             type: ['Temperature', 'CO2', 'Humidity', 'Pressure', 'Noise'],
         };
         return apiCallAsync(t.context.api, t.context.api.getMeasure, options).then(result => {
-            // console.log(result);
+            //console.log(result);
             t.assert(result);
             t.assert(Array.isArray(result));
+            t.assert(result[0].beg_time);
         }).catch(error => { t.is(error, ''); });
     });
 }
 
-if (getWeatherDeviceId() !== null) {
+if (getTestParameters().weatherDeviceId) {
     // @ts-ignore
-    test.serial('getRoomId with Weather Device', t => {
+    test.serial('getThermostatsData with Weather Device', t => {
         const options = {
-            device_id: getWeatherDeviceId(),
+            device_id: getTestParameters().weatherDeviceId,
         };
         return apiCallAsync(t.context.api, t.context.api.getThermostatsData, options).then(result => {
             // console.log(result);
@@ -116,6 +162,23 @@ if (getWeatherDeviceId() !== null) {
         }).catch(error => { t.is(error, 'getThermostatsDataError error: Device not found'); });
     });
 }
+
+/*
+device_id type is unknown function is deprecated by netatmo
+if (getTestParameters().getEnergyRelayDeviceId) {
+    // @ts-ignore
+    test.serial('getThermostatsData with EnergyRelayDevice Device', t => {
+        const options = {
+            device_id: getTestParameters().getEnergyRelayDeviceId,
+        };
+        return apiCallAsync(t.context.api, t.context.api.getThermostatsData, options).then(result => {
+            // console.log(result);
+            t.assert(result);
+            t.assert(Array.isArray(result));
+        }).catch(error => { t.is(error, 'getThermostatsDataError error: Device not found'); });
+    });
+}
+*/
 
 // @ts-ignore
 test.serial('getPublicData with Invalid coordinates and rain', t => {
@@ -131,7 +194,8 @@ test.serial('getPublicData with coordinates and temperature', t => {
         t.assert(result);
         // console.log(result);
         t.assert(Array.isArray(result));
-        t.is(result[0]._id, '70:ee:50:3c:2c:ac');
+        t.regex(result[0]._id, regexMacAddr, "[0]._id is not a mac address")
+        t.is(result[0]._id, '70:ee:50:3c:2c:ac'); // Dont worry, this id is public
     }).catch(error => { t.is(error, ''); });
 });
 
